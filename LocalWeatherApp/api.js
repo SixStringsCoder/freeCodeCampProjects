47447997644b1dfbd8c1f97d90806fb311f8fcbe
SixStringsCoder <rshanlon@gmail.com>
2018-01-24 23:45:14 -0800

    Added my own icons and bypassed icons in JSON since some don't show; still need to get main key words from JSON to use other icons, rainy, cloudy icons show fine since it's Portland

--- LocalWeatherApp/api.js
+++ LocalWeatherApp/api.js
@@ -1,4 +1,5 @@
 const x = document.getElementById("main");
+const weatherIcons = ["sunny", "rainy", "snowy", "cloudy", "partlyCloudy"];
 
 // Get GEO Location
 const getLocation = function() {
@@ -13,31 +14,60 @@ const getLocation = function() {
 const weatherApi = function(position) {
   const lon = position.coords.longitude;
   const lat = position.coords.latitude;
+  const key = '47fc843c8ccb0a9ebc311d1ee955da73';
+  //Using freecodecamp API
   const urlToFetch = `https://fcc-weather-api.glitch.me/api/current?lon=${lon}&lat=${lat}`;
+  // Using OpenWeatherMap api
+  // const urlToFetch = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${key}`;
+
   return fetch(urlToFetch)
     .then(response => response.json())
     .then(jsonResponse => {
+      console.log(jsonResponse);
       let weather = jsonResponse;
 
       renderWeather(weather);
     });
 }
 
+// For custom weather icons
+// function chooseIcon(weather) {
+//   weather = weather.weather[0].main
+//   if (weather.weather[0].main == "Rain") {
+//     $('.icon').html('<img src="./images/rainy.png" />');
+//   }
+// }
+
 function renderWeather(weather) {
-  console.log(weather);
+
   let fahrenheit = Math.round(weather.main.temp * 9/5 + 32) + '&#176;F';
   let celsius = Math.round(weather.main.temp) + '&#176;C';
-  let icon = weather.weather[0].icon;
   let description = weather.weather[0].description.toUpperCase();
-  let weatherIcon = `<img id="icon" src=${icon} alt="${description}" />`;
+  // let weatherIcon = `<img id="icon" src=${icon} alt="${description}" />`;
 
+    // add icons
+    if (weather.weather[0].main == "Rain") {
+      $('.icon').html('<img src="./images/rainy.png" />');
+    } else if (weather.weather[0].main == "Clouds") {
+      $('.icon').html('<img src="./images/cloudy.png" />');
+    }
+
+  $('.weatherResults').css("visibility", "visible");
   $('#fahrenheit').append(fahrenheit);
   $('#celsius').append(celsius);
-  $('.icon').append(weatherIcon);
   $('#description').append(description);
 }
 
+function clearScreen() {
+  // Clear out any existing weather info if present
+  $('.weatherResults').css("visibility", "hidden");
+  $('#fahrenheit').empty();
+  $('#celsius').empty();
+  $('.icon').empty();
+  $('#description').empty();
+}
 
 const howsTheWeather = function(){
-    getLocation();
-  }
+  clearScreen();
+  getLocation();
+}
