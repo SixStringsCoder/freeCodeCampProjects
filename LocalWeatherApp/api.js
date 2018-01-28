66a69b06b2eb9e5cfb0110a7466088a18c3954c9
SixStringsCoder <rshanlon@gmail.com>
2018-01-27 21:12:37 -0800

    Add Loading icon and add tweak JS to get it to show on Buttn click and disappear when rweather results show

--- LocalWeatherApp/api.js
+++ LocalWeatherApp/api.js
@@ -1,11 +1,18 @@
 const x = document.getElementById("main");
 const weatherIcons = ["sunny", "rainy", "snowy", "cloudy", "partlyCloudy"];
 
+// Spinning Icon while waiting for JSON call
+const waitIcon = () => {
+  $('#waitIcon').css("visibility", "visible");
+  console.log("Wait Icon!!")
+}
+
 // Get GEO Location
 const getLocation = function() {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(weatherApi);
+        setTimeout(waitIcon, 1300);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -17,8 +24,6 @@ const weatherApi = function(position) {
   const key = '47fc843c8ccb0a9ebc311d1ee955da73';
   //Using freecodecamp API
   const urlToFetch = `https://fcc-weather-api.glitch.me/api/current?lon=${lon}&lat=${lat}`;
-  // Using OpenWeatherMap api
-  // const urlToFetch = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${key}`;
 
   return fetch(urlToFetch)
     .then(response => response.json())
@@ -30,6 +35,7 @@ const weatherApi = function(position) {
     });
 }
 
+
 // For custom weather icons
 // function chooseIcon(weather) {
 //   weather = weather.weather[0].main
@@ -39,7 +45,7 @@ const weatherApi = function(position) {
 // }
 
 function renderWeather(weather) {
-
+  $('#waitIcon').css('visibility', 'hidden');
   let fahrenheit = Math.round(weather.main.temp * 9/5 + 32) + '&#176;F';
   let celsius = Math.round(weather.main.temp) + '&#176;C';
   let description = weather.weather[0].description.toUpperCase();
@@ -48,34 +54,33 @@ function renderWeather(weather) {
   let windSpeed = weather.wind.speed;
   const windIcon = '<img src="./images/wind.png" />';
 
+  // add weather icons
+  if (weather.weather[0].main === "Rain" || weather.weather[0].main === "Drizzle") {
+    $('.icon').html('<img src="./images/rainy.png" />');
+  } else if (weather.weather[0].main === "Clouds") {
+    $('.icon').html('<img src="./images/cloudy.png" />');
+  } else if (weather.weather[0].main === "Snow") {
+    $('.icon').html('<img src="./images/snowy.png" />');
+  } else if (weather.weather[0].main === "Sun") {
+    $('.icon').html('<img src="./images/sunny.png" />');
+  }
 
-    // add weather icons
-    if (weather.weather[0].main === "Rain" || weather.weather[0].main === "Drizzle") {
-      $('.icon').html('<img src="./images/rainy.png" />');
-    } else if (weather.weather[0].main === "Clouds") {
-      $('.icon').html('<img src="./images/cloudy.png" />');
-    } else if (weather.weather[0].main === "Snow") {
-      $('.icon').html('<img src="./images/snowy.png" />');
-    } else if (weather.weather[0].main === "Sun") {
-      $('.icon').html('<img src="./images/sunny.png" />');
-    }
-
-    // Determine wind direction text
-    if (windDir <= 78.75 && windDir >= 11.25) {
-      windText = 'NE';
-    } else if (windDir <= 101.25) {
-      windText = 'E';
-    } else if (windDir <= 191.25) {
-      windText = 'S';
-    } else if (windDir <= 258.75) {
-      windText = 'SW';
-    } else if (windDir <= 281.25) {
-      windText = 'W';
-    } else if (windDir < 348.75) {
-      windText = 'NW';
-    } else if (windDir > 348.75 || windDir < 11.25) {
-      windText = 'N';
-    }
+  // Determine wind direction text
+  if (windDir <= 78.75 && windDir >= 11.25) {
+    windText = 'NE';
+  } else if (windDir <= 101.25) {
+    windText = 'E';
+  } else if (windDir <= 191.25) {
+    windText = 'S';
+  } else if (windDir <= 258.75) {
+    windText = 'SW';
+  } else if (windDir <= 281.25) {
+    windText = 'W';
+  } else if (windDir < 348.75) {
+    windText = 'NW';
+  } else if (windDir > 348.75 || windDir < 11.25) {
+    windText = 'N';
+  }
 
   // Append to DOM
   $('.weatherResults').css("visibility", "visible");
@@ -87,6 +92,7 @@ function renderWeather(weather) {
 
 function clearScreen() {
   // Clear out any existing weather info if present
+  $('#waitIcon').css("visibility", "hidden");
   $('.weatherResults').css("visibility", "hidden");
   $('#fahrenheit').empty();
   $('#celsius').empty();
@@ -95,6 +101,7 @@ function clearScreen() {
   $('#wind').empty();
 }
 
+// Button is clicked to start the program
 const howsTheWeather = function(){
   clearScreen();
   getLocation();
