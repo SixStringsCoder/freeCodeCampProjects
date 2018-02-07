4e7eeaa64f1e9dc1f302ae31ae4a07f12d40dda9
SixStringsCoder <rshanlon@gmail.com>
2018-02-06 22:04:06 -0800

    Clarify AJAX endpoint and query string; adjust CSS Grid for results area

--- wikipedia_viewer/api.js
+++ wikipedia_viewer/api.js
@@ -1,14 +1,19 @@
 // Fetch info from wikipedia summarizing search term
 const getSummary = term => {
+  // Test Using Wikimedia REST API
   const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${term}`;
-  fetch(url)
+  // Test Using the MediaWiki API
+  // const search = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&gpssearch=${term}&gpslimit=10`;
+  const search = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms%7Cextracts&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=50&pilimit=10&wbptterms=description%7Clabel&gpssearch=${term}&gpslimit=10`;
+
+  fetch(search)
   .then(response => response.json())
   .then(jsonResponse => {
     console.log(jsonResponse);
   }).then(getRelated(term))
 }
 
-// Fetch info from wikipedia showing related results absed on getSummary() search term
+// Fetch info from wikipedia showing related results based on getSummary() search term
 const getRelated = term => {
   const url = `https://en.wikipedia.org/api/rest_v1/page/related/${term}`;
   fetch(url)
