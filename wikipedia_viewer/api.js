77cca99c3e1d1c0a25de96ca499cd2f7eeea30af
SixStringsCoder <rshanlon@gmail.com>
2018-02-07 23:58:36 -0800

    Add jQuery event handler to make AJAX call-returns 'Fetch failed loading' but works without event handler

--- wikipedia_viewer/api.js
+++ wikipedia_viewer/api.js
@@ -1,27 +1,35 @@
 // Fetch info from wikipedia summarizing search term
 const getSummary = term => {
+  const cors = 'https://cors-anywhere.herokuapp.com/'
   // Test Using Wikimedia REST API
-  const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${term}`;
+  // const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${term}`;
   // Test Using the MediaWiki API
-  // const search = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&gpssearch=${term}&gpslimit=10`;
-  const search = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms%7Cextracts&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=50&pilimit=10&wbptterms=description%7Clabel&gpssearch=${term}&gpslimit=10`;
-
-  fetch(search)
-  .then(response => response.json())
-  .then(jsonResponse => {
-    console.log(jsonResponse);
-  }).then(getRelated(term))
+  const search = `${cors}https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms%7Cextracts&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=50&pilimit=10&wbptterms=description%7Clabel&gpssearch=${term}&gpslimit=10`;
+  try {
+    fetch(search)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      console.log(jsonResponse);
+    })
+  } catch(error) {
+    console.error(error);
+  }
 }
 
 // Fetch info from wikipedia showing related results based on getSummary() search term
-const getRelated = term => {
-  const url = `https://en.wikipedia.org/api/rest_v1/page/related/${term}`;
-  fetch(url)
-  .then(response => response.json())
-  .then(jsonResponse => {
-    console.log(jsonResponse)
-  })
-}
+// const getRelated = term => {
+//   const url = `https://en.wikipedia.org/api/rest_v1/page/related/${term}`;
+//   fetch(url)
+//   .then(response => response.json())
+//   .then(jsonResponse => {
+//     console.log(jsonResponse)
+//   })
+// }
 
-// Test call
-getSummary("guitar");
+
+// Click button to call getSummary function based on input's value
+$('#submit').on('click', function() {
+    const searchTerm = $('#input').val();
+    console.log(searchTerm);
+    getSummary(searchTerm);
+})
