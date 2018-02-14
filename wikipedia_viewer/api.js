f79fc7bca4d006a0eb5bed8c88332968af30530e
SixStringsCoder <rshanlon@gmail.com>
2018-02-13 22:35:11 -0800

    Adjust CSS Grid for smallest screens

--- wikipedia_viewer/api.js
+++ wikipedia_viewer/api.js
@@ -33,16 +33,17 @@ const renderDetails = (response) => {
 
   const results = response.query.pages;
   results.map((result, index) => {
-    let learnLink = `https://en.wikipedia.org/wiki/${results[index].title}`;
+    let title = results[index].title.replace(/\s+/g, '_'); // replaces spaces with underscore so that weblink works
+    let learnLink = `https://en.wikipedia.org/wiki/${title}`;
     let image = results[index].thumbnail ? results[index].thumbnail.source : "No Image Available";
     let extract = results[index].extract ? results[index].extract : "No extract available";
     let text = results[index].terms.description ? results[index].terms.description[0] : results[index].terms.label[0];
     let article = `
     <article class="result">
       <h3 class="resultTitle">${results[index].title}</h3>
-      <img class="resultImage" src=${image} alt=${results[index].title} />
+      <div><img class="resultImage" src=${image} alt=${results[index].title} /></div>
       <p class="resultText"><span>Summary:</span> ${text}</p>
-      <button class="learn"><a href=${learnLink} target="_blank">Learn More</a></button>
+      <a class="learn" href=${learnLink} target="_blank">Learn More</a>
     </article>`;
     $('#searchResults').append(article);
   });
@@ -52,8 +53,9 @@ const renderDetails = (response) => {
 
 // Click button to call getSummary function with input's value as the argument
 $('#btn').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     const searchTerm = $('#term').val();
     console.log(searchTerm);
     getSummary(searchTerm);
+    $('#term').reset();
 });
