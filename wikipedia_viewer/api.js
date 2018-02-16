ff739254a4d99bd04491f5f47dd39303ff0926a2
SixStringsCoder <rshanlon@gmail.com>
2018-02-15 23:54:28 -0800

    Add no-image JPEG for entries without image; add extract to fill out description area

--- wikipedia_viewer/api.js
+++ wikipedia_viewer/api.js
@@ -2,10 +2,9 @@
 const getSummary = term => {
   const cors = 'https://cors-anywhere.herokuapp.com/';
   const wikiUrl = 'https://en.wikipedia.org';
-  // Test Using Wikimedia REST API
-  // const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${term}`;
-  // Test Using the MediaWiki API
-  const search = `${cors}${wikiUrl}/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms%7Cextracts&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=150&pilimit=10&wbptterms=description%7Clabel&exsentences=2&gpssearch=${term}&gpslimit=10`;
+
+  // Fetch JSON using the MediaWiki API
+  const search = `${cors}${wikiUrl}/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms%7Cimageinfo%7Cextracts%7Cimages&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=150&pilimit=10&wbptterms=description%7Clabel&exsentences=2&exlimit=3&exintro=1&explaintext=1&exsectionformat=plain&gpssearch=${term}&gpslimit=10`;
   try {
     fetch(search)
     .then(response => response.json())
@@ -18,13 +17,13 @@ const getSummary = term => {
   }
 }
 
-// Fetch info from wikipedia showing related results based on getSummary() search term
+// Fetch JSON using REST API from wikipedia showing related results based on getSummary() search term
 // const getRelated = term => {
-//   const url = `https://en.wikipedia.org/api/rest_v1/page/related/${term}`;
+//   const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${term}`;
 //   fetch(url)
 //   .then(response => response.json())
-//   .then(jsonResponse => {
-//     console.log(jsonResponse)
+//   .then(jsonResponse2 => {
+//     console.log(jsonResponse2)
 //   })
 // }
 
@@ -35,14 +34,14 @@ const renderDetails = (response) => {
   results.map((result, index) => {
     let title = results[index].title.replace(/\s+/g, '_'); // replaces spaces with underscore so that weblink works
     let learnLink = `https://en.wikipedia.org/wiki/${title}`;
-    let image = results[index].thumbnail ? results[index].thumbnail.source : "No Image Available";
-    let extract = results[index].extract ? results[index].extract : "No extract available";
+    let image = results[index].thumbnail ? results[index].thumbnail.source : "./images/no-image.jpg";
+    let extract = results[index].extract ? results[index].extract : "No further extract available";
     let text = results[index].terms.description ? results[index].terms.description[0] : results[index].terms.label[0];
     let article = `
     <article class="result">
       <h3 class="resultTitle">${results[index].title}</h3>
       <div><img class="resultImage" src=${image} alt=${results[index].title} /></div>
-      <p class="resultText"><span>Summary:</span> ${text}</p>
+      <p class="resultText"><span>Summary:</span> ${text}; ${extract}</p>
       <a class="learn" href=${learnLink} target="_blank">Learn More</a>
     </article>`;
     $('#searchResults').append(article);
