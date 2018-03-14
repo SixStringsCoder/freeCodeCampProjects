44677a36618bbb5e0cd9a3f92f50264ca1918c35
SixStringsCoder <rshanlon@gmail.com>
2018-03-13 21:58:08 -0700

    Finished fetch function for 'featured' JSON; started adding search input and button to search for specific username login names

--- twitchTVApi/api.js
+++ twitchTVApi/api.js
@@ -6,72 +6,56 @@ const x = document.getElementById("main");
 //   console.log("Wait Icon!!")
 // }
 
+
+
 const twitchApi = function() {
   const cors = 'https://cors-anywhere.herokuapp.com/';
   const url = 'https://wind-bow.gomix.me/twitch-api/streams/';
+  const users = 'https://wind-bow.glitch.me/twitch-api/users/';
   const summary = 'summary';
   const featured = 'featured';
+  const user = "freecodecamp";
   const urlToFetch = `${cors}${url}${featured}`;
 
   return fetch(urlToFetch)
     .then(response => response.json())
     .then(jsonResponse => {
-      console.log(jsonResponse);
       let tvGuide = jsonResponse.featured;
-      tvGuide.map(entry => {
-        return {
-          image: entry.image,
-          name: entry.stream.game,
-          followers: entry.stream.channel.followers,
-          text: entry.text,
-          logo: entry.stream.channel.logo,
-          preview: entry.stream.preview.medium,
-          url: entry.stream.channel.url,
-        }
-      })
+      console.log(jsonResponse);
+      renderTvGuide(tvGuide);
     });
+}
+
+
+function renderTvGuide(channels) {
+  console.log(channels);
+  channels.map(entry => {
+
+    let channelRow =
+      `<section class="channel">
+        <div>
+          <div class="topInfo">
+            <img class="logo" src=${entry.stream.channel.logo} alt="logo" />
+            <h3 class="gameName">${entry.stream.game}</h3>
+            <p class="followers">followers: ${entry.stream.channel.followers}</p>
+          </div>
+
+          <img class="icon" src=${entry.image} />
+          <div class="description">
+            ${entry.text}
+          </div>
+
+          <a class="viewChannelBtn" href=${entry.stream.channel.url} target="_blank"><button>View Channel</button></a>
+        </div>
+      </section>`;
+
+    main.innerHTML += channelRow;
+  });
+  return channels;
 }
 
 twitchApi();
 
-// function renderWeather(weather) {
-//   $('#waitIcon').css('visibility', 'hidden');
-//   let fahrenheit = Math.round(weather.main.temp * 9/5 + 32) + '&#176;F';
-//   let celsius = Math.round(weather.main.temp) + '&#176;C';
-//   let description = weather.weather[0].description.toUpperCase();
-//   const windDir = weather.wind.deg;
-//   let windText = '';
-//   let windSpeed = weather.wind.speed;
-//   const windIcon = '<img src="./images/wind.png" />';
-//
-//   // add weather icons
-//   if (weather.weather[0].main === "Rain" || weather.weather[0].main === "Drizzle") {
-//     $('.icon').html('<img src="./images/rainy.png" />');
-//   } else if (weather.weather[0].main === "Clouds") {
-//     $('.icon').html('<img src="./images/cloudy.png" />');
-//   } else if (weather.weather[0].main === "Snow") {
-//     $('.icon').html('<img src="./images/snowy.png" />');
-//   } else if (weather.weather[0].main === "Sun") {
-//     $('.icon').html('<img src="./images/sunny.png" />');
-//   }
-//
-//   // Determine wind direction text
-//   if (windDir <= 78.75 && windDir >= 11.25) {
-//     windText = 'NE';
-//   } else if (windDir <= 101.25) {
-//     windText = 'E';
-//   } else if (windDir <= 191.25) {
-//     windText = 'S';
-//   } else if (windDir <= 258.75) {
-//     windText = 'SW';
-//   } else if (windDir <= 281.25) {
-//     windText = 'W';
-//   } else if (windDir < 348.75) {
-//     windText = 'NW';
-//   } else if (windDir > 348.75 || windDir < 11.25) {
-//     windText = 'N';
-//   }
-//
   // Append to DOM
 //   $('.weatherResults').css("visibility", "visible");
 //   $('#fahrenheit').append(fahrenheit);
